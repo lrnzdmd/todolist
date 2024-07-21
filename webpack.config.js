972b29965120c65d4b61cfdb094b467f157e22c0
lrnzdmd <170991996+lrnzdmd@users.noreply.github.com>
2024-07-21 04:41:31 +0200

    added the logo/home button to the page, and of course, bird.

--- webpack.config.js
+++ webpack.config.js
@@ -13,5 +13,21 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
 }
